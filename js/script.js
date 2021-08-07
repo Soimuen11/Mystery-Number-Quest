c09bc20ddefaaf6dc93233bd917509a2d572aa29
Phil Wayne <soimuen11@gmail.com>
2021-08-07 15:49:24 +0200

    implemented a better-looking popup to get player name

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
-// define mystery number
+// Define mystery number
 const mystery = Math.ceil(Math.random() * 100)
-console.log(mystery)
-// number of attemps in one game
-let attempts = 0;
-// get the name of current player
-let player_name = prompt("Hello mate! Just enter a name alright!")
-let panel_body1 = document.getElementById("panel-body1")
-panel_body1.innerHTML = player_name
 
+// Number of attemps in one game
+let attempts = 0;
+
+// initializing player name with empty string
+let player_name = new String();
+
+// DOM manipulation
+let panel_body1 = document.getElementById("panel-body1")
 let button = document.getElementById("btn1");
 let attempt_area = document.getElementById("panel-body2")
 let advice_zone = document.getElementById("advice_zone")
-// click event loop
+
+
+// Get the name of current player
+swal("Please enter your name:", {
+	content: "input",
+})
+	.then((value) => {
+		swal(`Welcome ${value}`);
+		panel_body1.innerHTML = value
+		player_name = value
+		if (value == null || value.length == 0){
+			panel_body1.innerHTML = "Anonymous"
+		}
+});
+
+// Click event loop
 button.addEventListener("click", function(){
 	let player_input = document.getElementById("player_input").value
 	let player_number = parseInt(player_input)
@@ -22,9 +38,8 @@ button.addEventListener("click", function(){
 	}else if (player_number > mystery){
 		advice_zone.innerHTML = "Lower than that!"
 	}else if(player_number == mystery){
-		swal("Hey " + player_name + "!", " You have just won in " + attempts + " attempts!")
-	}
-	else{
+		swal("Well done " + player_name + "!", " You have won in " + attempts + " attempts!")
+	}else{
 		advice_zone.innerHTML = "I don't get it. You sure it's a number?"
 	}
 })
